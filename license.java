4086ff34269ffe9952dae163b2185db3ac7cc30e
icehuhu <icehuhu@foxmail.com>
2015-04-23 15:31:42 +0800

    Update license.java

--- license.java
+++ license.java
@@ -3,6 +3,10 @@ package license;
 public class license {
 	private int InitialLicenseNum =0;
 	private int CalculateNum =0;
+	public void ChangeInitialNum(int num)
+	{
+		InitialLicenseNum=num;
+	}
 	public boolean JudgeServiceRequest()
 	{
 		CalculateNum+=1;
